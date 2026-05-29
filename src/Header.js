e4f02d463638bccaa6a80c946bca44048ea84844
ouchan <kingbook0628@gmail.com>
2026-05-29 10:14:16 -0700

    ヘッダーとcssを作った

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return <header>
+    <div className='header'>
+      <h3>ポートフォリオ</h3>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">ブログ</a></li>
+            <li><a href="#">SNS</a></li>
+
+        </ul>
+    </nav>
+  </header>
+}
+
+export default Header
